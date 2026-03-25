9595ccc6868a666f0c88090eb60b6eedccd5ee3b
JFUENTES91 <larajael338@gmail.com>
2026-03-25 16:31:33 -0600

    Añadi Practica2

---
+++ Unidad1/src/Practica2/src/practica2/Practica2.java
@@ -0,0 +1,21 @@
+package practica2;
+
+import practica2.Login;
+import java.awt.Window;
+/**
+ *
+ * @author Joha
+ */
+public class Practica2 {
+
+    public static void main(String[] args) {
+  
+ 
+     Login Login_p = new Login();   
+       
+      Login_p.setVisible(true);
+
+     
+    }
+    
+}
