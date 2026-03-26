35129da45a14f0422b689fbbc2c901c7945adda6
JFUENTES91 <larajael338@gmail.com>
2026-03-25 23:48:55 -0600

    Anadi Practica1

---
+++ Unidad1/src/Practica1/src/practica1/Practica1.java
@@ -0,0 +1,37 @@
+
+package practica1;
+
+import practica1.Figuras;
+import practica1.Circulo;
+import practica1.Cuadrado;
+import practica1.Rectangulo;
+import practica1.Triangulo;
+
+
+/**
+ *
+ * @author Joha
+ */
+public class Practica1 {
+    public static void main(String[] args) {
+   
+       Figuras Figuras_p = new Figuras();
+       Figuras_p.setVisible(true);
+        
+       Circulo Circulo_p = new Circulo();
+       Circulo_p.setVisible(true);
+        
+       Cuadrado Cuadrado_p = new Cuadrado();
+       Cuadrado_p.setVisible(true);
+        
+       Rectangulo Rectangulo_p = new Rectangulo();
+       Rectangulo_p.setVisible(true);
+       
+       Triangulo Triangulo_p = new Triangulo();
+       Triangulo_p.setVisible(true);
+       
+       
+       
+    }
+    
+}
